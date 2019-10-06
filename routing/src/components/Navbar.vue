8bee512f06dd4f7256b8f82c121a742495d0887e
AlenaNik <alena.nik.digital@gmail.com>
2019-10-06 19:14:15 +0200

    user id params passed to a navbar

--- routing/src/components/Navbar.vue
+++ routing/src/components/Navbar.vue
@@ -1,8 +1,16 @@
 <template>
   <nav class="main-nav">
     <ul>
-        <li><router-link to="/home">Home</router-link></li>
+       <li><router-link to="/home">Home</router-link></li>
       <li><router-link :to="{ name: 'About' }">About</router-link></li>
+    </ul>
+    <h2>User Profiles</h2>
+    <ul>
+      <li v-for="(id, index) in userIds" :key="index">
+        <router-link :to="{ name: 'Profile', params: { user_id: id}}">
+          <span>Profile {{ id }}</span>
+        </router-link>
+      </li>
     </ul>
   </nav>
 </template>
@@ -11,7 +19,7 @@ export default {
   name: 'Navbar',
   data() {
     return {
-
+      userIds: [ '1', '2', '3', '4']
     }
   }
 }
