c9e98eaf937d664d013a7694c08c647eb9536b74
AlenaNik <alena.nik.digital@gmail.com>
2019-10-06 10:54:44 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <Navbar/>
+    <h1>{{ title }}</h1>
+  </div>
+</template>
+
+<script>
+import Navbar from "./Navbar.component";
+
+export default {
+  name: 'app',
+  components: {
+    Navbar
+  },
+  data () {
+    return {
+      title: 'Vue App, woo :)'
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+
+h1  {
+  font-weight: normal;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
