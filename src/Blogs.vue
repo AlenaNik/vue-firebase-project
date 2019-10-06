a60ae2d92dd13dc1413ccd71beef6818ffb14cbf
AlenaNik <alena.nik.digital@gmail.com>
2019-10-06 15:53:28 +0200

    filtering spaceships

--- src/Blogs.vue
+++ src/Blogs.vue
@@ -1,11 +1,12 @@
 <template>
   <div class="blogs">
-    <h2 class="font-bold bg-blue-100 border-t border-b border-blue-500 text-blue-700 px-4 py-3"> Starships </h2>
-    <div v-for="ship in starships" :key="ship.name">
 
+    <h2 class="font-bold bg-indigo-100 border-t border-b border-blue-500 text-blue-700 px-4 py-3"> Starships </h2>
+    <input class="border-b border-blue-500" type="text" v-model="searchTerm">
+    <div v-for="ship in filteredShips" :key="ship.name">
       <h3 class="text-blue-600">{{ ship.name }}</h3>
       <p class="text-pink-600">{{ ship.model }}</p>
-      <p class="text-pink-800">{{ ship.manufacturer }}</p>
+      <p class="text-pink-800">{{ ship.manufacturer | snippet}}</p>
 
     </div>
   </div>
@@ -16,11 +17,19 @@ import axios from 'axios'
      name: 'Blogs',
      data() {
        return {
-          starships: []
+          starships: [],
+          searchTerm: ''
        }
      },
      methods: {
 
+     },
+     computed: {
+       filteredShips(){
+         return  this.starships.filter(ship => {
+           return ship.name.match(this.searchTerm)
+         })
+       }
      },
      created() {
        axios.get('https://swapi.co/api/starships?format=json')
--- src/OnlineFriends.component.vue
+++ src/OnlineFriends.component.vue
@@ -2,7 +2,7 @@
   <div id="online-friends">
     <h2 class="font-bold bg-blue-100 border-t border-b border-blue-500 text-blue-700 px-4 py-3">Online Friends</h2>
     <div v-for="(friend, index) in friends" :key="index">
-      <span class="center text-red-600" v-if="friend.online === true"> {{ friend.name }}</span>
+      <span class="center text-green-600" v-if="friend.online === true"> {{ friend.name }}</span>
     </div>
   </div>
 </template>
