48bae1625011415c6ae49d895eb55eb3328fde06
AlenaNik <alena.nik.digital@gmail.com>
2019-10-08 15:48:22 +0200

    outputting dummy data

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <Navbar />
+    <router-view/>
+    <Index/>
+  </div>
+</template>
+
+<script>
+import Navbar from "./components/Navbar.component";
+import Index from "./components/Index";
+export default {
+  name: 'App',
+  components: {
+    Index,
+    Navbar
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Index.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="index container">
+    <div class="card" v-for="day in days" :key="day.id">
+      <div  class="card-content">
+          <p class="black-text">{{ day.title }}</p>
+           <ul class="activities">
+            <li v-for="(d, index) in day.activities" :key="index">
+              <span class="chip">{{ d }}</span>
+            </li>
+          </ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Index',
+  data () {
+    return {
+      days: [
+        { title: 'Rainy Day', slug: 'rainy-day', activities: ['stay inside', 'coffee', 'movie'], id: 1},
+        { title: 'Sunny Day', slug: 'sunny-day', activities: ['go outside', 'coffee to go', 'park'], id: 2}
+      ]
+    }
+  }
+}
+</script>
+
+
+<style scoped>
+
+</style>
