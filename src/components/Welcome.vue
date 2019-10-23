15b615af2935729faa33192ebf0d2539f88d6817
AlenaNik <alena.nik.digital@gmail.com>
2019-10-23 17:01:53 +0200

    typing in a form

--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -2,12 +2,11 @@
   <div class="welcome container">
     <div class="card">
         <div class="card-content center-align">
-            <h2 class="teal-text">Vue</h2>
-            <form @submit.prevent="startChatting">
+            <h2 class="teal-text">Main</h2>
+            <form @submit.prevent="enterChat">
             <label for="name">Enter your name:</label>
             <input type="text" name="name" v-model="name">
             <button
-            @click="submitTheForm"
             class="btn teal"
             >Start the chat</button>
           </form>
@@ -34,4 +33,14 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style>
+  .welcome {
+    max-width: 400px;
+    margin-top: 100px;
+  }
+  .welcome h2 {
+    font-size: 3em;
+  }
+  .welcome button {
+    margin: 40px;
+  }
 </style>
