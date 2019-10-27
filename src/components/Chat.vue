fe3d7e185e12ff4af8edc8fec7c66feccbae0d5f
AlenaNik <alena.nik.digital@gmail.com>
2019-10-27 18:36:53 +0100

    getting a message

--- src/components/Chat.vue
+++ src/components/Chat.vue
@@ -1,13 +1,36 @@
 <template>
-  <div class="chat container">Chat window
-      <p>Hello, {{ this.name }}</p>
+  <div class="chat container">
+    <h2 class="center teal-text"> Chat Window </h2>
+      <div class="card">
+        <div class="card-content">
+          <ul class="chatmessages">
+            <li>
+              <span class="teal-text">
+                {{ this.name }}
+              </span>
+              <span class="grey-text text-darken-3">
+                message content
+              </span>
+              <span class="grey-text time">time</span>
+            </li>
+          </ul>
+        </div>
+        <div class="card-action">
+          <Message :name="name"/>
+        </div>
+      </div>
   </div>
 </template>
 
 <script>
+  import Message from './Message';
+
   export default {
     name: 'Chat',
     props: ['name'],
+    components: {
+      Message
+    },
     data() {
       return {
       }
@@ -15,5 +38,15 @@
   }
 </script>
 <style>
+.chat h2 {
+  font-size: 2.6em;
+  margin-bottom: 40px;
+}
+.chat span {
+  font-size: 1.4em;
+}
 
+.chat .time {
+  display: block;
+}
 </style>
---
+++ src/components/Message.vue
@@ -0,0 +1,25 @@
+<template>
+<div class="message">
+  <form @submit.prevent="addMessage">
+    <label for="message">New chat message (enter to add)</label>
+    <input type="text" name="message" v-model="Message">
+  </form>
+</div>
+</template>
+
+<script>
+  export default {
+    name: 'Message',
+    props: ['name'],
+    data() {
+      return {
+        Message: null
+      }
+    },
+    methods: {
+      addMessage() {
+        console.log(this.Message, this.name, Date.now())
+      }
+    }
+  }
+</script>
