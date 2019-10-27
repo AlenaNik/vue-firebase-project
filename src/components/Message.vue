f24e27460825eeb922ff0874d5946bb576e07408
AlenaNik <alena.nik.digital@gmail.com>
2019-10-27 18:54:30 +0100

    firebase getting messegaes

--- src/components/Message.vue
+++ src/components/Message.vue
@@ -3,22 +3,33 @@
   <form @submit.prevent="addMessage">
     <label for="message">New chat message (enter to add)</label>
     <input type="text" name="message" v-model="Message">
+    <p v-if="feedback" class="red-text"> {{ feedback }}</p>
   </form>
 </div>
 </template>
 
 <script>
+import db from '../firebase/init';
+
   export default {
     name: 'Message',
     props: ['name'],
     data() {
       return {
-        Message: null
+        Message: null,
+        feedback: null,
       }
     },
     methods: {
       addMessage() {
-        console.log(this.Message, this.name, Date.now())
+        console.log(this.Message, this.name, Date.now());
+        (this.Message) ?
+          db.collection('messages').add({
+            content: this.Message,
+            name: this.name,
+            timestamp: Date.now()
+          })
+          : (this.feedback = "Enter a message")
       }
     }
   }
